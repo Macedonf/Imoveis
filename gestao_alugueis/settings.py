7e5f1752ee4eaf6777c19efbc6ec288068f8942a
Macedonf <matheus.joaonf@gmail.com>
2024-10-30 17:49:09 -0300

    aula-04

--- gestao_alugueis/settings.py
+++ gestao_alugueis/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'login'
+
 
 # Application definition
 
@@ -116,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
